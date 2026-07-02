17330b13d47bf44ae9b00e61c5acf8d9ae0b1f05
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-02 14:06:37 +0530

    Create Q2.java

---
+++ Assignment_37/Q2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Bitwise
+{
+    public int OffBit(int iNo, int iPos)
+    {
+        int iMask = 0x1;
+        int iRet = 0;
+
+        iMask = iMask << (iPos - 1);
+
+        iMask = ~iMask;
+
+        iRet = iNo & iMask;
+        return iRet;
+    }
+}
+
+public class Q2
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        Bitwise bobj = new Bitwise();
+
+        int iValue = 0;
+        int iPosition = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iValue = sobj.nextInt();
+
+        System.out.println("Enter position : ");
+        iPosition = sobj.nextInt();
+
+        iRet = bobj.OffBit(iValue, iPosition);
+
+        System.out.println("Updated no  : " + iRet);
+        
+        sobj.close();
+    }
+}
