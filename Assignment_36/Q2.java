8a30cdd4931a08437bf758869d62cabab0c588b8
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-02 13:56:51 +0530

    Create Q2.java

---
+++ Assignment_36/Q2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Q2
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iMask1 = 0x1;
+        int iMask2 = 0x1;
+        int iMask = 0;
+        int iNo = 0;
+        int iRet = 0;
+
+        System.out.println("Enter number : ");
+        iNo = sobj.nextInt();
+
+        iMask1 = iMask1 << 6;
+
+        iMask2 = iMask2 << 9;
+
+        iMask = iMask1 | iMask2;
+
+        iMask = ~iMask;
+
+        iRet = iNo & iMask;
+
+        System.out.println("Updated no  : " + iRet);
+        
+        sobj.close();
+    }
+}
