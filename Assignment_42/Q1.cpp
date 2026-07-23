7caf9455a5a682441791eec8acb010012dda0ac6
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:44:41 +0530

    Update Q1.cpp

--- Assignment_42/Q1.cpp
+++ Assignment_42/Q1.cpp
@@ -1,1 +1,22 @@
 // Assignment 42 - Q1
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Multiply(T no1, T no2)
+{
+    T ans;
+    ans = no1 * no2;
+    return ans;
+}
+
+int main()
+{
+    int iRet = Multiply(10,20);
+    cout<<"Multiplication of Integers : "<<iRet<<endl;
+
+    float fRet = Multiply(10.0f,20.0f);
+    cout<<"Multiplication of Floats : "<<fRet<<endl;
+
+    return 0;
+}
