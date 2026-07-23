4d0ddfc769e8bbea17a1d6c37a2da9b99ce0b6b8
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:46:38 +0530

    Update Q5.cpp

--- Assignment_42/Q5.cpp
+++ Assignment_42/Q5.cpp
@@ -1,1 +1,33 @@
 // Assignment 42 - Q5
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Min(T *arr, int iSize)
+{
+    T min = arr[0];
+
+    for(int i = 1; i < iSize; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    float brr[] = {10.0f,3.7f,9.8f,8.7f};
+
+    int iRet = Min(arr,5);
+    cout<<"Smallest Integer : "<<iRet<<endl;
+
+    float fRet = Min(brr,4);
+    cout<<"Smallest Float : "<<fRet<<endl;
+
+    return 0;
+}
