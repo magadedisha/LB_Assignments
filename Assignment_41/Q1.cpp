1ebd0699a76ae2f1fbc1dfa2e57844c0844182ec
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:42:39 +0530

    Update Q1.cpp

--- Assignment_41/Q1.cpp
+++ Assignment_41/Q1.cpp
@@ -1,1 +1,22 @@
 // Assignment 41 - Q1
+#include<iostream>
+using namespace std;
+
+template<class T>
+void Display(T value, int iSize)
+{
+    for(int i = 0; i < iSize; i++)
+    {
+        cout << value << "\t";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Display('M',7);
+    Display(11,3);
+    Display(3.7,6);
+
+    return 0;
+}
