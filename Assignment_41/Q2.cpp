f520d6fceddae47a53a2f5d359096762b27e0887
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:43:09 +0530

    Update Q2.cpp

--- Assignment_41/Q2.cpp
+++ Assignment_41/Q2.cpp
@@ -1,1 +1,30 @@
 // Assignment 41 - Q2
+#include<iostream>
+using namespace std;
+
+template<class T>
+int Frequency(T *arr, int iSize, T iNo)
+{
+    int iCount = 0;
+
+    for(int i = 0; i < iSize; i++)
+    {
+        if(arr[i] == iNo)
+        {
+            iCount++;
+        }
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+
+    int iRet = Frequency(arr,9,10);
+
+    cout<<"Frequency is : "<<iRet<<endl;
+
+    return 0;
+}
