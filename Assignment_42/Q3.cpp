d3dac1f86c231cecbb729e391282e7194656873b
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:45:36 +0530

    Update Q3.cpp

--- Assignment_42/Q3.cpp
+++ Assignment_42/Q3.cpp
@@ -1,1 +1,30 @@
 // Assignment 42 - Q3
+#include<iostream>
+using namespace std;
+
+template<class T>
+T AddN(T *arr, int iSize)
+{
+    T sum = 0;
+
+    for(int i = 0; i < iSize; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    float brr[] = {10.0f,3.7f,9.8f,8.7f};
+
+    int iSum = AddN(arr,5);
+    cout<<"Integer Sum : "<<iSum<<endl;
+
+    float fSum = AddN(brr,4);
+    cout<<"Float Sum : "<<fSum<<endl;
+
+    return 0;
+}
