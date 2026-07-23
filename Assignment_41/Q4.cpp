30df94b3a8056424e44407476fb60bac3eecb881
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:43:52 +0530

    Update Q4.cpp

--- Assignment_41/Q4.cpp
+++ Assignment_41/Q4.cpp
@@ -1,1 +1,28 @@
 // Assignment 41 - Q4
+#include<iostream>
+using namespace std;
+
+template<class T>
+int SearchFirst(T *arr, int iSize, T iNo)
+{
+    for(int i = 0; i < iSize; i++)
+    {
+        if(arr[i] == iNo)
+        {
+            return i + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+
+    int iRet = SearchFirst(arr,9,40);
+
+    cout<<"First occurrence : "<<iRet<<endl;
+
+    return 0;
+}
