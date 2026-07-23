596ad172209bb04699a312fc8ea344b27778b89f
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:44:13 +0530

    Update Q5.cpp

--- Assignment_41/Q5.cpp
+++ Assignment_41/Q5.cpp
@@ -1,1 +1,43 @@
 // Assignment 41 - Q5
+#include<iostream>
+using namespace std;
+
+template<class T>
+void Reverse(T *arr, int iSize)
+{
+    int start = 0;
+    int end = iSize - 1;
+
+    while(start < end)
+    {
+        T temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+
+    cout<<"Before Reverse : ";
+
+    for(int i = 0; i < 9; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    Reverse(arr,9);
+
+    cout<<"\nAfter Reverse : ";
+
+    for(int i = 0; i < 9; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
