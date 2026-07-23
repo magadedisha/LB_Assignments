e1d392f9e28afcdbca3a169955691923a638e0d8
magadedisha <146059179+magadedisha@users.noreply.github.com>
2026-07-23 13:45:00 +0530

    Update Q2.cpp

--- Assignment_42/Q2.cpp
+++ Assignment_42/Q2.cpp
@@ -1,1 +1,29 @@
 // Assignment 42 - Q2
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Max(T no1, T no2, T no3)
+{
+    T max = no1;
+
+    if(no2 > max)
+    {
+        max = no2;
+    }
+
+    if(no3 > max)
+    {
+        max = no3;
+    }
+
+    return max;
+}
+
+int main()
+{
+    cout<<"Largest Integer : "<<Max(10,20,30)<<endl;
+    cout<<"Largest Float : "<<Max(5.6f,7.8f,2.3f)<<endl;
+
+    return 0;
+}
